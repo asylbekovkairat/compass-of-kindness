36591c043e622f6268fb415cd4e5ba167beca465
asylbekovkairat <asyl06kair@gmail.com>
2024-07-19 12:05:56 +0600

    fix

--- front/src/shared/lib/router/types.ts
+++ front/src/shared/lib/router/types.ts
@@ -1,12 +1,12 @@
 export const RoutesUrls = {
-  root: `/${import.meta.env.VITE_BASE}/`,
-  login: `/${import.meta.env.VITE_BASE}/login`,
+  root: `/${import.meta.env.VITE_ROUTER_BASE}/`,
+  login: `/${import.meta.env.VITE_ROUTER_BASE}/login`,
 
-  allCharities: `/${import.meta.env.VITE_BASE}/all-charities`,
-  needHelp: `/${import.meta.env.VITE_BASE}/need-help`,
-  aboutProject: `/${import.meta.env.VITE_BASE}/about-project`,
-  helpProject: `/${import.meta.env.VITE_BASE}/help-project`,
+  allCharities: `/${import.meta.env.VITE_ROUTER_BASE}/all-charities`,
+  needHelp: `/${import.meta.env.VITE_ROUTER_BASE}/need-help`,
+  aboutProject: `/${import.meta.env.VITE_ROUTER_BASE}/about-project`,
+  helpProject: `/${import.meta.env.VITE_ROUTER_BASE}/help-project`,
 
-  logout: `/${import.meta.env.VITE_BASE}/cabinet/logout`,
-  notFound: `/${import.meta.env.VITE_BASE}/not-found`,
+  logout: `/${import.meta.env.VITE_ROUTER_BASE}/cabinet/logout`,
+  notFound: `/${import.meta.env.VITE_ROUTER_BASE}/not-found`,
 };
